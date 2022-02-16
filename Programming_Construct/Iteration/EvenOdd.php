42bb4ba71ba40abbc7153cef3adc863a873ac551
akshaydig <you@example.com>
2022-02-16 10:02:47 +0530

    Added: Programming Construct Programs in PHP

---
+++ Programming_Construct/Iteration/EvenOdd.php
@@ -0,0 +1,8 @@
+<?php  
+$number = readline('Enter the number :');  
+if($number%2==0){  
+    echo "$number is even number";  
+}else{  
+    echo "$number is odd number";  
+}  
+?>
---
+++ Programming_Construct/Sequence/EvenOdd.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0){
+        echo "\n Even Number : ".$i;
+    } else {
+        echo "\n Odd Number : ".$i;
+    }
+}
+
+?>
