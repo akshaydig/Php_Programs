afe8b998dc1ff739a40402f070cc40870d13ea5f
akshaydig <you@example.com>
2022-02-16 09:59:50 +0530

    Added: Basic Core Programs in PHP

---
+++ Basic_core_Programs/LeapYear.php
@@ -0,0 +1,13 @@
+<?php
+$a = readline('Enter the Number : ');
+
+    // If the given condition true then
+    if ($a % 4 == 0){
+
+        echo "Number is Leap Year"; //True
+    } else {
+
+        echo "Number is Not Leap Year"; //False
+    }
+
+?>
